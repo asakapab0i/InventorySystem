70fdb6788fca47d59be460d4561ead35d81b9b97
Bryan <bryan@inventory-sys.com>
2012-06-24 18:07:39 +0800

    Added an index.php and some css to, beautify the whole index.php, restructured all the codes and tags for better readability.

--- index.php
+++ index.php
@@ -1,13 +1,43 @@
+<?php
+include 'connections/connection.php';
+include 'includes/header.php';
+include 'includes/footer.php';
+?>
 <!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" href="assets/css/main.css">
+        <link rel="stylesheet" href="assets/css/bigbutton.css">
+        <script type="text/javascript" src="assets/js/date_time.js"></script>
         <title>Welcome to Homepage</title>
     </head>
     <body>
-        <?php
-        // put your code here
-        echo 'Hello World';
-        ?>
+        <div id = "wrapper">
+            <header id="main_header">
+                <div id="rightAlign">    
+                    <?php
+                    mainHeader();
+                    ?>
+                </div>
+                <a href="index.php"><img src="assets/images/mainLogo2.png"></a>
+            </header>
+            <div id="main_section">
+                <?php
+                if (isset($_SESSION['username'])) {
+                    echo '<br/><br/><br/><br/><br/><h3>Inventory Portal</h3>';
+                    echo '<h3><a href="../inventory/employee/home.php" class="bigbutton">HOME</a></h3>';
+                } else {
+                    echo '<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><h3><a href="login.php" class="bigbutton">LOGIN</a></h3>';
+                }
+                ?>
+            </div>
+            <div id="main_footer" class="links">
+                <?php mainFooter(); ?>
+            </div>
+
+
+
+        </div>
     </body>
 </html>
