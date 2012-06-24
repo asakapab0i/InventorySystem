4a80d0880dbb5b33bb62629559ff7c97608429ab
Bryan <bryan@inventory-sys.com>
2012-06-24 19:42:42 +0800

    added a Object Oriented Method to cleans the data to about SQL injections from outside sources.

---
+++ library/datacleansing.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Cleanse the data to prevent SQL injection from outside sources.
+ * Object Oriented Style again!
+ * @author Bryan Bojorque
+ */
+class DataCleansing {
+
+    //put your code here
+    private $_data;
+
+    public function StripAndEscape($data) {
+        $this->_data = $data;
+        
+        $this->_data = stripcslashes($this->_data);
+        $this->_data = mysql_escape_string($this->_data);
+
+        return $this->_data;
+    }
+}
+
+$cleanse = new DataCleansing();
+
+?>
