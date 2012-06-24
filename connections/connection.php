7fc72eab3ca7368dfe16e52bd17b2e2d3705d73b
Bryan <bryan@inventory-sys.com>
2012-06-24 16:59:29 +0800

    Added a connection to the database. Object Oriented Style!

---
+++ connections/connection.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * @desc Database connection set up, Object Oriented Style! 
+ * @author Bryan Bojorque
+ * 
+ */
+class Database {
+
+    public $host = "localhost";
+    public $username = "root";
+    public $password = "";
+    public $database = "InventorySystem";
+
+    public function __construct() {
+        $this->MysqlCheckConnection();
+        $this->MysqCheckSelection();
+    }
+
+    public function MysqlCheckConnection() {
+
+        /**
+         * @desc Check if the mysql_connect is succesful or not. 
+         */
+        if (!mysql_connect($this->host, $this->username, $this->password)) {
+            die("Cannot connect to Database, please fix Connection/ folder.");
+        } else {
+            return true;
+        }
+    }
+
+    public function MysqCheckSelection() {
+
+        /**
+         * @desc Check if there is a database to select. 
+         */
+        if (!mysql_select_db($this->database)) {
+            die("Cannot select a Database, Database $this->database does not exist please fix Connection/ folder.");
+        } else {
+            return true;
+        }
+    }
+
+}
+
+$connect = new Database();
+?>
